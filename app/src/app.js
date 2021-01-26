2554d273d280961b74e5f80f5c15d10f996815ce
Tiago Garcia <tiagojsag@gmail.com>
2021-01-26 12:42:46 +0100

    Replace CT integration library

--- app/src/app.js
+++ app/src/app.js
@@ -8,7 +8,7 @@ const koaValidate = require('koa-validate');
 const koaBody = require('koa-body');
 const koaSimpleHealthCheck = require('koa-simple-healthcheck');
 const ErrorSerializer = require('serializers/errorSerializer');
-const ctRegisterMicroservice = require('ct-register-microservice-node');
+const { RWAPIMicroservice } = require('rw-api-microservice-node');
 
 // instance of koa
 const app = new Koa();
@@ -61,28 +61,31 @@ koaValidate(app);
 
 app.use(koaSimpleHealthCheck());
 
+app.use(RWAPIMicroservice.bootstrap({
+    name: config.get('service.name'),
+    info: require('../microservice/register.json'),
+    swagger: require('../microservice/public-swagger.json'),
+    logger,
+    baseURL: process.env.CT_URL,
+    url: process.env.LOCAL_URL,
+    token: process.env.CT_TOKEN,
+    fastlyEnabled: process.env.FASTLY_ENABLED,
+    fastlyServiceId: process.env.FASTLY_SERVICEID,
+    fastlyAPIKey: process.env.FASTLY_APIKEY
+}));
+
 // load routes
 loader.loadRoutes(app);
 
-// Instance of http module
 const server = app.listen(process.env.PORT, () => {
-    ctRegisterMicroservice.register({
-        info: require('../microservice/register.json'),
-        swagger: require('../microservice/public-swagger.json'),
-        mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
-        framework: ctRegisterMicroservice.KOA2,
-        app,
-        logger,
-        name: config.get('service.name'),
-        ctUrl: process.env.CT_URL,
-        url: process.env.LOCAL_URL,
-        token: process.env.CT_TOKEN,
-        active: true
-    }).then(() => {
-    }, (error) => {
-        logger.error(error);
-        process.exit(1);
-    });
+    if (process.env.CT_REGISTER_MODE === 'auto') {
+        RWAPIMicroservice.register().then(() => {
+            logger.info('CT registration process started');
+        }, (error) => {
+            logger.error(error);
+            process.exit(1);
+        });
+    }
 });
 
 
