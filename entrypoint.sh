52823c78d98187ec2996150b6865fc50234c7bf4
Solomon Negusse <solomon.negusse@wri.org>
2023-11-13 13:51:11 +0400

    update dev environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        exec grunt --gruntfile app/Gruntfile.js
+        exec yarn run dev
         ;;
     startDev)
         echo "Running Start Dev"
